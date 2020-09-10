2ae0bafe85000d5f2f303fba855c409a765fa2f3
rensi <rensimeila04@gmail.com>
2020-09-10 17:14:43 +0700

    StyleAndThemeUpdated
    Signed-off-by: Your Name <you@example.com>

--- app/src/main/java/com/smktelkom/www/myviewandviews/MainActivity.java
+++ app/src/main/java/com/smktelkom/www/myviewandviews/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("Google Pixel");
+            getSupportActionBar().setTitle("Black Shark 3");
         }
     }
 }
